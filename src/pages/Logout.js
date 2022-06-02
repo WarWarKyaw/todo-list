6d3657cd8b0072a3e89b4aaf34ed219f2445e8a7
War War Kyaw <warwar67890@gmail.com>
2022-06-02 22:11:46 +0630

    fixed logout ui

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Logout() {
   const origin = window.location.origin;
   return (
-    <div>
-      <h3>ログアウトになりました。</h3>
-      <h4 style={{ marginTop: 20 }}>
+    <div style={{ backgroundColor: "#E9ECEF", padding: 30, borderRadius: 5 }}>
+      <h4 style={{ fontSize: 35 }}>ログアウトになりました。</h4>
+      <h5 style={{ marginTop: 20, fontSize: 20 }}>
         こちらに<a href={origin}>ログイン</a> できる!
-      </h4>
+      </h5>
     </div>
   );
 }
